dee8516cec92a12b8f2e86d73abff89f04cbeb62
zibiao-zhang <118461117+zibiao-zhang@users.noreply.github.com>
2026-09-18 23:07:44 -0400

    Add Firebase configuration for competition app

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDwhTKmmotw-3TSmf5JYMDwiGVOIEcy6A0",
+  authDomain: "bq-john-quiz.firebaseapp.com",
+  databaseURL: "https://bq-john-quiz-default-rtdb.firebaseio.com",
+  projectId: "bq-john-quiz",
+  storageBucket: "bq-john-quiz.firebasestorage.app",
+  messagingSenderId: "242020443110",
+  appId: "1:242020443110:web:fa16372564e182ff1c6c27"
+};
